c8e4cca42889c7cb60ec0925ba4b96ac7fe764a4
llxiaoh <644932921@qq.com>
2017-03-14 21:58:12 +0800

    something change

--- doubanbook/items.py
+++ doubanbook/items.py
@@ -9,6 +9,20 @@ import scrapy
 
 
 class DoubanbookItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+	#书名
+	bookName = scrapy.Field()
+	#作者
+	author = scrapy.Field()
+	#评分
+	score = scrapy.Field()
+	#评分人数
+	scoreCount = scrapy.Field()
+	#出版社
+	publishCompany = scrapy.Field()
+	#出版时间
+	publishTime = scrapy.Field()
+	#链接
+	link = scrapy.Field()
+	#简介
+	desc = scrapy.Field()
+
